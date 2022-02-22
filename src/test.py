21a4d6d89621e0e1fe3866eaaf7e8ada1189b572
Sean Ostermann <sean.ostermann@bellmts.ca>
2022-02-22 06:32:03 -0600

    Remove test.py and add pyinstaller outputs to .gitignore.  Fix conditional in screensaver.py to use != rather than not

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import cairosvg
-from io import BytesIO
-from urllib.request import Request, urlopen
-
-print("running")
-
-urllib.request.urlretrieve("https://cdn.nhle.com/logos/nhl/svg/BOS_light.svg", "local-filename.svg")
-
-print("Done !")
